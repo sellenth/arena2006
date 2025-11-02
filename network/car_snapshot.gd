bf376bb109a9055b89187d3818d79100d0d17637
halston <halstonsellentin@gmail.com>
2025-11-02 11:16:22 -0800

    cleanup 2

--- network/car_snapshot.gd
+++
@@ -1,7 +0,0 @@
-extends RefCounted
-class_name CarSnapshot
-
-var tick := 0
-var transform := Transform3D.IDENTITY
-var linear_velocity := Vector3.ZERO
-var angular_velocity := Vector3.ZERO
--- scenes/visualizations/spring/scaler_pivot.gd
+++
@@ -1,8 +0,0 @@
-@tool
-extends Marker3D
-
-@onready var start_pos := Vector3.ZERO
-
-func _physics_process(_delta: float) -> void:
-	var diff = 1 - (%WheelMesh.global_position.y - start_pos.y)
-	scale.y = diff
